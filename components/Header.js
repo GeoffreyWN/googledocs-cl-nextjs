ae19a8eeb15e7f09f4f68dfa7003d0efa8980c4a
weshsnr <ndungugeoffrey11@gmail.com>
2021-07-10 21:21:41 +0300

    added material tailwind

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="flex items-center">
+                        
+        </div>
+    )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Google Docs Cl</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h2>get started</h2>
+      <Header/>
     </div>
   )
 }
