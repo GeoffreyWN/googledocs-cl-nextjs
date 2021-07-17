b25ba0b7426e22d21abb962d66dc622086538b90
weshsnr <ndungugeoffrey11@gmail.com>
2021-07-17 13:15:13 +0300

    added add doc section

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,55 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Header from '../components/Header'
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Google Docs Cl</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
-        />
       </Head>
       <Header />
+
+      <section className='bg-[#F8F9FA]  pb-10 px-10'>
+        <div className="max-w-3xl mx-auto ">
+          <div className="flex items-center justify-between py-6">
+            <h2 className="text-gray-700 text-lg " >Start a new Doc</h2>
+            <Button
+              color="gray"
+              buttonType="outline"
+              iconOnly={true}
+              rounded={true}
+              ripple="dark"
+              className=" border-0"
+            >
+              <Icon name='more_vert' size="3xl" />
+            </Button>
+          </div>
+
+          <div className="">
+            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700" >
+              <Image src="https://links.papareact.com/pju" layout="fill" />
+            </div>
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700 " >Blank</p>
+          </div>
+
+        </div>
+      </section>
+
+      <section className="bg-white px-10 md:px-0 ">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700 " >
+          <div className=" flex items-center justify-between pb-5 ">
+            <h2 className="font-medium flex-grow" >My Documents</h2>
+            <p className="mr-12" >Date Created</p>
+            <Icon name='folder' size="3xl" color="gray" />
+          </div>
+        </div>
+      </section>
+
     </div>
   )
 }
